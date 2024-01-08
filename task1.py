de2ad1cf5e633d8236568c81dce3f663a6a9c796
Artem <104737575+Aneyadisonna@users.noreply.github.com>
2024-01-08 17:42:07 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,3 +1,10 @@
+'''
+Task 1
+how many complete rows of coins (with increasing numbers of coins in each row) can be placed with a given number of coins `num`
+'''
+
+
+# first solution
 def arrangeCoins(num):
     step = 0
     coins = 0
@@ -9,5 +16,13 @@ def arrangeCoins(num):
     return step - 1
 
 
+# second solution
+def arrangeCoins2(num):
+    return int(((8 * num + 1) ** 0.5 - 1) // 2)
+
+
 print(arrangeCoins(5))
 print(arrangeCoins(8))
+
+print(arrangeCoins2(5))
+print(arrangeCoins2(8))
